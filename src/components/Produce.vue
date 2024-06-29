4d09287d554aa4c3fdf1df3d526f2716e703aedc
Carolina Margiotti <55335180+CarolinaMargiotti@users.noreply.github.com>
2024-06-28 22:21:26 -0300

    created produce and basic setup for clicker

---
+++ src/components/Produce.vue
@@ -0,0 +1,27 @@
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+	name: "Produce",
+	props: {
+		produceName: {
+			type: String,
+			required: true,
+			default: "Cucumber",
+		},
+	},
+	computed: {
+		produceImage() {
+			if (!this.produceName) return "";
+			return `../../../public/${this.produceName}.png`;
+		},
+	},
+});
+</script>
+
+<template>
+	<div>
+		{{ produceName }}
+		<img class="w-14" :src="produceImage" :alt="produceName" />
+	</div>
+</template>
--- src/views/GuineaPigClicker.vue
+++ src/views/GuineaPigClicker.vue
@@ -1,5 +1,19 @@
-<template>
-	<div class="bg-yellow-950">porqinhodaindia</div>
-</template>
+<script setup>
+import Produce from "@/components/Produce.vue";
+</script>
 
-<script></script>
+<template>
+	<div class="grid grid-cols-2 justify-center items-center">
+		<div>
+			<div class="bg-yellow-950">porqinhodaindia</div>
+			<div class="w-28">
+				<img src="../../public/guineapig.png" alt="guinea farmer" />
+			</div>
+		</div>
+		<div class="min-content">
+			<li class="produce min-content">
+				<Produce produceName="Cucumber"></Produce>
+			</li>
+		</div>
+	</div>
+</template>
